a61c646099dd473c3a2d7afc89d4b608b0e2325e
konradcodes <konkordek47@gmail.com>
2019-04-05 21:11:07 +0100

    Comments for tasksView functions added

--- src/js/views/tasksView.js
+++ src/js/views/tasksView.js
@@ -1,1 +1,8 @@
 import { elements } from "./base";
+
+//* When row is clicked - show input to add your task, hide bin and + icons. Add button and cancel button
+
+
+//* When bin is clicked delete the row. Ask a confirm message
+
+//*When completed dot is clicked line-through the text animation + darken the row.
